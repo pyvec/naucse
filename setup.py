7fc28b2dd206cbfa5f3790f257054c419d1f5370
Petr Viktorin <encukou@gmail.com>
2019-08-18 15:54:52 +0200

    Replace pipenv with setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from setuptools import setup
+
+setup(
+    name='naucse',
+    version='0.1',
+    description='Website for course materials',
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    author='Petr Viktorin & others',
+    author_email='encukou@gmail.com',
+    license='MIT',
+    license_files=['LICENSE.MIT'],
+    url='https://github.com/encukou/naucse',
+    packages=['naucse'],
+    install_requires=[
+        'elsa',
+        'ics',
+        'python-dateutil',
+        'arca[docker]',
+        'cssutils',
+        'PyYAML',
+        'Flask',
+        'Jinja2',
+        'Werkzeug',
+        'jsonschema',
+        'lxml',
+        'naucse-render',
+    ],
+    zip_safe=False,
+)
